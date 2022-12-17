4d4823918902ed1fa1a9c00bc2212e0f55d62258
Kei Kaloustian <kei.kaloustian@gmail.com>
2022-12-17 07:36:33 -0800

    Build preliminary Allocation comp

--- src/Components/Allocation.jsx
+++ src/Components/Allocation.jsx
@@ -1,11 +1,20 @@
+import { useState } from "react";
+
 export default function Allocation() {
-  const assets = [0, 1, 2];
+  // const assets = [0, 1, 2];
+  const [assets, setAssets] = useState([]);
+
+  const addAsset = (newAsset) => {
+    setAssets([...assets, newAsset]);
+  };
 
   return (
-    <>
+    <section>
+      <h1>What do you want to invest in?</h1>
       {assets.map((a) => (
         <p>{a}</p>
       ))}
-    </>
+      <button onClick={() => addAsset("test")}>Add</button>
+    </section>
   );
 }
