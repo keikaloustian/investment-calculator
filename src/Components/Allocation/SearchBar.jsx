9bac3eedeed3e114ebd40ba23cd03f0a7b070c48
Kei Kaloustian <kei.kaloustian@gmail.com>
2022-12-17 12:46:05 -0800

    Create SearchBar comp

---
+++ src/Components/Allocation/SearchBar.jsx
@@ -0,0 +1,3 @@
+export default function SearchBar() {
+  return <input placeholder="Search ticker..."></input>;
+}
--- src/Components/Allocation/index.jsx
+++ src/Components/Allocation/index.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import Asset from "../Asset";
+import SearchBar from "./SearchBar";
 
 export default function Allocation() {
   // const assets = [0, 1, 2];
@@ -12,7 +13,7 @@ export default function Allocation() {
   return (
     <section>
       <h1>What do you want to invest in?</h1>
-      <input></input>
+      <SearchBar></SearchBar>
       <button onClick={() => addAsset("test")}>Add</button>
       <ul>
         {assets.map((a) => (
