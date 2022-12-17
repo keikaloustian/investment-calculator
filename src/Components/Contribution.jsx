1ee36893c8b373bd93b5831e3a9a6cf5f4a18aad
Kei Kaloustian <kei.kaloustian@gmail.com>
2022-12-16 22:28:20 -0800

    Build preliminary input investment amount form and buttons

--- src/Components/Contribution.jsx
+++ src/Components/Contribution.jsx
@@ -1,8 +1,25 @@
+import { useState } from "react";
+
 export default function Contribution() {
+  const [amount, setAmount] = useState();
+
   return (
     <>
       <h1>How much do you want to invest?</h1>
-      <input type={"number"} min={1} step={50}></input>
+      <button onClick={() => setAmount(100)}>$100</button>
+      <button onClick={() => setAmount(500)}>$500</button>
+      <button onClick={() => setAmount(1000)}>$1000</button>
+
+      <div>
+        <div className="adornment">$</div>
+        <input
+          type={"number"}
+          min={0}
+          step={100}
+          value={amount}
+          onChange={(e) => setAmount(e.target.value)}
+        ></input>
+      </div>
     </>
   );
 }
