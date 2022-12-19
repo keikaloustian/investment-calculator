31b4ea7c86e1fd629c7be7dd4d35b70d48090f39
Kei Kaloustian <kei.kaloustian@gmail.com>
2022-12-19 13:47:47 -0800

    Make AsyncSelect into controlled component with state for the selected assets

--- src/components/Allocation/SearchBar.jsx
+++ src/components/Allocation/SearchBar.jsx
@@ -2,12 +2,20 @@ import { useEffect, useState } from "react";
 import AsyncSelect from "react-select/async";
 
 export default function SearchBar() {
+  const [selected, setSelected] = useState([]);
+
+  const handleChange = (selectedAssets) => {
+    // AsyncSelect component provides argument
+    // which can be set as new state directly
+    setSelected(selectedAssets);
+  };
+
   const promiseOptions = (inputValue) => {
     return fetch(`https://api.publicapis.org/entries?title=${inputValue}`)
       .then((response) => response.json())
       .then((data) =>
         data.entries.map((e) => ({
-          value: e.API,
+          value: e,
           label: e.API,
         }))
       );
@@ -15,8 +23,12 @@ export default function SearchBar() {
 
   return (
     <AsyncSelect
-      placeholder={"Search asset"}
+      placeholder={"Search assets..."}
+      value={selected}
+      onChange={handleChange}
       loadOptions={promiseOptions}
+      openMenuOnClick={false}
+      isMulti
       isClearable
     />
   );
