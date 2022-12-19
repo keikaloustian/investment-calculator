433f68b7f5e556cd3a2896642c943e67e340572b
Kei Kaloustian <kei.kaloustian@gmail.com>
2022-12-18 21:00:50 -0800

    Build preliminary autocomplete search bar with test API

--- src/components/Allocation/SearchBar.jsx
+++ src/components/Allocation/SearchBar.jsx
@@ -1,11 +1,16 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import AsyncSelect from "react-select/async";
 
 export default function SearchBar() {
   const promiseOptions = (inputValue) => {
     return fetch(`https://api.publicapis.org/entries?title=${inputValue}`)
       .then((response) => response.json())
-      .then((data) => data);
+      .then((data) =>
+        data.entries.map((e) => ({
+          value: e.API,
+          label: e.API,
+        }))
+      );
   };
 
   return (
