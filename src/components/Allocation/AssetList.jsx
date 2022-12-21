f922ae99066954635b2c28d1540a1e5830c401fe
Kei Kaloustian <kei.kaloustian@gmail.com>
2022-12-20 19:48:10 -0800

    Ensure amount is being passed from App down to AssetList

--- src/components/Allocation/AssetList.jsx
+++ src/components/Allocation/AssetList.jsx
@@ -4,6 +4,7 @@ export default function AssetList(props) {
   // List of asset cards
   return (
     <ul>
+      <li>{props.amount}</li>
       {props.assets.map((a) => (
         <AssetCard asset={a}></AssetCard>
       ))}
