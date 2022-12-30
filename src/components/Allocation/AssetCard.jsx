47375eed948aa3c3adfc673c3aff739fa483e3f4
Kei Kaloustian <kei.kaloustian@gmail.com>
2022-12-29 17:59:33 -0800

    Make allocation slider into controlled input

--- src/components/Allocation/AssetCard.jsx
+++ src/components/Allocation/AssetCard.jsx
@@ -1,7 +1,13 @@
+import { useState } from "react";
 import useApiPrice from "../../hooks/useApiPrice";
 
 export default function AssetCard({ data }) {
   const { price, error } = useApiPrice(data.symbol, data.exchange);
+  const [allocation, setAllocation] = useState(0);
+
+  const handleSlider = (event) => {
+    setAllocation(event.target.value);
+  };
 
   return (
     <li>
@@ -11,7 +17,14 @@ export default function AssetCard({ data }) {
       <span>PRICE: {price ? price : "loading..."}</span>
       <label>
         Allocation (%)
-        <input type={"range"} min={0} max={100} />
+        {allocation}
+        <input
+          type={"range"}
+          min={0}
+          max={100}
+          value={allocation}
+          onChange={handleSlider}
+        />
       </label>
       {error && <span>{error}</span>}
     </li>
