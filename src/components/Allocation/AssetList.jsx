857dec714060cce1681e56e1121420de6048dc13
Kei Kaloustian <kei.kaloustian@gmail.com>
2022-12-29 19:12:08 -0800

    Move export context statement outside component

--- src/components/Allocation/AssetList.jsx
+++ src/components/Allocation/AssetList.jsx
@@ -1,10 +1,11 @@
 import { createContext, useState } from "react";
 import AssetCard from "./AssetCard";
 
+export const AllocationContext = createContext();
+
 export default function AssetList({ assets, setAssets, amount }) {
   // State and context to manage the allocation of funds onto the assets
   const [remainder, setRemainder] = useState(100);
-  const AllocationContext = createContext();
 
   return (
     // Provider that gives asset cards access to the remainder to be allocated
