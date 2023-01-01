f79741d2f9045fd391631df11249a44c37718fe3
Kei Kaloustian <kei.kaloustian@gmail.com>
2022-12-31 17:03:18 -0800

    Change element that wraps error from span to p

--- src/components/Allocation/AssetCard.jsx
+++ src/components/Allocation/AssetCard.jsx
@@ -48,7 +48,7 @@ export default function AssetCard({ data }) {
           </div>
         </label>
       </div>
-      {error && <span>{error}</span>}
+      {error && <p>{error}</p>}
     </li>
   );
 }
