aded637952f74715c43b8108b34754a05e78dc57
Kei Kaloustian <kei.kaloustian@gmail.com>
2023-01-02 20:29:02 -0800

    Refactor - make handleSlider a separate helper function

--- src/components/Allocation/AssetCard.jsx
+++ src/components/Allocation/AssetCard.jsx
@@ -1,6 +1,7 @@
 import { useState, useContext } from "react";
 import useApiPrice from "../../hooks/useApiPrice";
 import { AllocationContext } from "./AssetList";
+import handleSlider from "../../helpers/handleSlider";
 
 export default function AssetCard({ data }) {
   const { price, error } = useApiPrice(data.symbol, data.country);
@@ -8,24 +9,6 @@ export default function AssetCard({ data }) {
 
   // Access AllocationContext provided by AssetList
   const { remainder, setRemainder } = useContext(AllocationContext);
-
-  const handleSlider = (event) => {
-    // Convert the value of the input of type range from string to number
-    const newValue = Number(event.target.value);
-
-    // Handle increase (slide to right)
-    if (newValue > value) {
-      if (remainder > 0) {
-        setRemainder((prev) => prev - (newValue - value));
-        setSliderValue(newValue);
-      }
-    }
-    // Handle decrease (slide to left)
-    if (newValue < value) {
-      setSliderValue(newValue);
-      setRemainder((prev) => prev + (value - newValue));
-    }
-  };
 
   return (
     <li>
@@ -43,7 +26,15 @@ export default function AssetCard({ data }) {
               min={0}
               max={100}
               value={value}
-              onInput={handleSlider}
+              onInput={(event) =>
+                handleSlider(
+                  event,
+                  value,
+                  remainder,
+                  setRemainder,
+                  setSliderValue
+                )
+              }
             />
           </div>
         </label>
