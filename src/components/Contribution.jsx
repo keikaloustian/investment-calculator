2086ab06561a23b86b71318886a5ac6aa10aa669
Kei Kaloustian <kei.kaloustian@gmail.com>
2023-01-07 16:42:00 -0800

    Style Allocate button, wrap amount buttons in div

--- src/components/Contribution.jsx
+++ src/components/Contribution.jsx
@@ -5,15 +5,20 @@ export default function Contribution({ amount, setAmount }) {
   return (
     <section className="contribution">
       <h1 className="contribution__heading">How much do you want to invest?</h1>
-      <button className="button button--left" onClick={() => setAmount(100)}>
-        $100
-      </button>
-      <button className="button" onClick={() => setAmount(500)}>
-        $500
-      </button>
-      <button className="button button--right" onClick={() => setAmount(1000)}>
-        $1000
-      </button>
+      <div className="button__group">
+        <button className="button button--left" onClick={() => setAmount(100)}>
+          $100
+        </button>
+        <button className="button" onClick={() => setAmount(500)}>
+          $500
+        </button>
+        <button
+          className="button button--right"
+          onClick={() => setAmount(1000)}
+        >
+          $1000
+        </button>
+      </div>
 
       <div className="input">
         <div className="input input__adornment">$</div>
@@ -24,6 +29,7 @@ export default function Contribution({ amount, setAmount }) {
           value={amount}
           onChange={(e) => setAmount(e.target.value)}
         ></input>
+        <button className="button button--allocate">Allocate</button>
       </div>
     </section>
   );
