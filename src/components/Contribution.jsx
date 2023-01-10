97d285f0eb8aff70d8d1cf188a995332d2660301
Kei Kaloustian <kei.kaloustian@gmail.com>
2023-01-09 18:47:25 -0800

    Refactor Contribution styling and scss to use mixins

--- src/components/Contribution.jsx
+++ src/components/Contribution.jsx
@@ -5,31 +5,29 @@ export default function Contribution({ amount, setAmount }) {
   return (
     <section className="contribution">
       <h1 className="contribution__heading">How much do you want to invest?</h1>
-      <div className="button__group">
-        <button className="button button--left" onClick={() => setAmount(100)}>
+      <div className="button-group">
+        <button className="button--left" onClick={() => setAmount(100)}>
           $100
         </button>
-        <button className="button" onClick={() => setAmount(500)}>
+        <button className="button--middle" onClick={() => setAmount(500)}>
           $500
         </button>
-        <button
-          className="button button--right"
-          onClick={() => setAmount(1000)}
-        >
+        <button className="button--right" onClick={() => setAmount(1000)}>
           $1000
         </button>
       </div>
 
-      <div className="input">
-        <div className="input input__adornment">$</div>
+      <div className="amount">
+        <div className="amount__adornment">$</div>
         <input
+          className="amount__input"
           type={"number"}
           min={0}
           step={100}
           value={amount}
           onChange={(e) => setAmount(e.target.value)}
         ></input>
-        <button className="button button--allocate">Allocate</button>
+        <button className="button--allocate">Allocate</button>
       </div>
     </section>
   );
