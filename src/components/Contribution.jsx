986d0639c0804904be5284d20a2518d18b33bbd4
Kei Kaloustian <kei.kaloustian@gmail.com>
2023-01-09 21:06:24 -0800

    Complete basic Contribution style

--- src/components/Contribution.jsx
+++ src/components/Contribution.jsx
@@ -5,29 +5,30 @@ export default function Contribution({ amount, setAmount }) {
   return (
     <section className="contribution">
       <h1 className="contribution__heading">How much do you want to invest?</h1>
-      <div className="button-group">
-        <button className="button--left" onClick={() => setAmount(100)}>
-          $100
-        </button>
-        <button className="button--middle" onClick={() => setAmount(500)}>
-          $500
-        </button>
-        <button className="button--right" onClick={() => setAmount(1000)}>
-          $1000
-        </button>
-      </div>
-
-      <div className="input-group">
-        <div className="input-group__adornment">$</div>
-        <input
-          className="input-group__input"
-          type={"number"}
-          min={0}
-          step={100}
-          value={amount}
-          onChange={(e) => setAmount(e.target.value)}
-        ></input>
-        <button className="button--allocate">Allocate</button>
+      <div className="split">
+        <div className="button-group">
+          <button className="button--left" onClick={() => setAmount(100)}>
+            $100
+          </button>
+          <button className="button--middle" onClick={() => setAmount(500)}>
+            $500
+          </button>
+          <button className="button--right" onClick={() => setAmount(1000)}>
+            $1000
+          </button>
+        </div>
+        <div className="input-group">
+          <span className="input-group__adornment">$</span>
+          <input
+            className="input-group__input"
+            type={"number"}
+            min={0}
+            step={100}
+            value={amount}
+            onChange={(e) => setAmount(e.target.value)}
+          ></input>
+          <button className="button--allocate">Allocate</button>
+        </div>
       </div>
     </section>
   );
