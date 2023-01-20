3e1b2101e2d2d3fa71c67c96ccd327d15c5de2d6
Kei Kaloustian <kei.kaloustian@gmail.com>
2023-01-19 21:10:07 -0800

    Remove uneeded div

--- src/components/Contribution.jsx
+++ src/components/Contribution.jsx
@@ -13,38 +13,36 @@ export default function Contribution({
   return (
     <section className="contribution">
       <h1 className="contribution__heading">How much do you want to invest?</h1>
-      <div className="split">
-        <div className="button-group">
-          <button className="button--left" onClick={() => setAmount(100)}>
-            $100
-          </button>
-          <button className="button--middle" onClick={() => setAmount(500)}>
-            $500
-          </button>
-          <button className="button--right" onClick={() => setAmount(1000)}>
-            $1000
-          </button>
-        </div>
-        <form className="input-group">
-          <span className="input-group__adornment">$</span>
-          <input
-            className="input-group__input"
-            type={"number"}
-            min={0}
-            step={100}
-            value={amount}
-            onChange={(event) => setAmount(event.target.value)}
-            autoFocus
-          ></input>
-          <button
-            className="button--allocate"
-            onClick={toggleAllocationVisible}
-            type="button"
-          >
-            Allocate
-          </button>
-        </form>
+      <div className="button-group">
+        <button className="button--left" onClick={() => setAmount(100)}>
+          $100
+        </button>
+        <button className="button--middle" onClick={() => setAmount(500)}>
+          $500
+        </button>
+        <button className="button--right" onClick={() => setAmount(1000)}>
+          $1000
+        </button>
       </div>
+      <form className="input-group">
+        <span className="input-group__adornment">$</span>
+        <input
+          className="input-group__input"
+          type={"number"}
+          min={0}
+          step={100}
+          value={amount}
+          onChange={(event) => setAmount(event.target.value)}
+          autoFocus
+        ></input>
+        <button
+          className="button--allocate"
+          onClick={toggleAllocationVisible}
+          type="button"
+        >
+          Allocate
+        </button>
+      </form>
     </section>
   );
 }
