07124bf8c14408e9d5337427eac7f1b372b55468
Kei Kaloustian <kei.kaloustian@gmail.com>
2023-01-21 16:13:56 -0800

    Pass remainder and setRemainder as props instead of using context

--- src/components/Allocation/AssetList.jsx
+++ src/components/Allocation/AssetList.jsx
@@ -1,26 +1,28 @@
-import { createContext, useState } from "react";
+import { useState } from "react";
 import AssetCard from "./AssetCard";
 import "./AssetList.scss";
-
-// Context to manage remaining percent of funds to allocate
-export const AllocationContext = createContext();
 
 export default function AssetList({ assets, amount }) {
   // State to manage remaining percent of funds to allocate
   const [remainder, setRemainder] = useState(100);
 
   return (
-    // Provider that gives access to the remainder % to be allocated
-    <AllocationContext.Provider value={{ remainder, setRemainder }}>
+    <>
       <p className="remainder-msg">
         You have <b>{remainder}%</b> of ${amount} left to allocate
       </p>
       {/* List of asset cards */}
       <ul className="asset-list">
         {assets.map((asset, index) => (
-          <AssetCard data={asset.value} key={index} amount={amount}></AssetCard>
+          <AssetCard
+            data={asset.value}
+            key={index}
+            amount={amount}
+            remainder={remainder}
+            setRemainder={setRemainder}
+          ></AssetCard>
         ))}
       </ul>
-    </AllocationContext.Provider>
+    </>
   );
 }
