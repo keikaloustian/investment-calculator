007942c35d141623cb646c61e34d37868bef300f
Kei Kaloustian <kei.kaloustian@gmail.com>
2023-01-27 12:24:51 -0800

    Create basic loading dots component with style and animation

---
+++ src/components/Allocation/LoadingDots.jsx
@@ -0,0 +1,11 @@
+import "./LoadingDots.scss";
+
+export default function LoadingDots() {
+  return (
+    <div className="loader">
+      <span className="loader__dot"></span>
+      <span className="loader__dot"></span>
+      <span className="loader__dot"></span>
+    </div>
+  );
+}
