6502e62ec6586ababc9833585b589322168bec2d
Kei Kaloustian <kei.kaloustian@gmail.com>
2023-02-11 18:50:36 -0800

    Make contribution amount buttons focus input onClick via ref

--- src/components/Contribution.jsx
+++ src/components/Contribution.jsx
@@ -1,3 +1,4 @@
+import { useRef } from "react";
 import "./Contribution.scss";
 
 import { MdAttachMoney } from "react-icons/md";
@@ -13,17 +14,43 @@ export default function Contribution({
     setAllocationVisible(true);
   };
 
+  // Ref for contribution input
+  const contributionRef = useRef(null);
+
+  // Function that focuses on contribution input using ref
+  const focusContribution = () => {
+    contributionRef.current.focus();
+  };
+
   return (
     <section className="contribution">
       <h1 className="contribution__heading">How much do you want to invest?</h1>
       <div className="button-group">
-        <button className="button--left" onClick={() => setAmount(100)}>
+        <button
+          className="button--left"
+          onClick={() => {
+            setAmount(100);
+            focusContribution();
+          }}
+        >
           $100
         </button>
-        <button className="button--middle" onClick={() => setAmount(500)}>
+        <button
+          className="button--middle"
+          onClick={() => {
+            setAmount(500);
+            focusContribution();
+          }}
+        >
           $500
         </button>
-        <button className="button--right" onClick={() => setAmount(1000)}>
+        <button
+          className="button--right"
+          onClick={() => {
+            setAmount(1000);
+            focusContribution();
+          }}
+        >
           $1000
         </button>
       </div>
@@ -34,6 +61,7 @@ export default function Contribution({
             <MdAttachMoney></MdAttachMoney>
           </span>
           <input
+            ref={contributionRef}
             className="input-group__input"
             type={"number"}
             min={0}
